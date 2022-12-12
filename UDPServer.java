476541b1033a79e9ce0854f8826e0bed5347b993
André Oliveira <58999171+AndreOliveira23@users.noreply.github.com>
2022-12-12 00:24:38 -0300

    Update UDPServer.java
    Atualização UDPServer: Refatoração do código, adição de comentários e formatação do ACK (acompanhando o segundo commit do UDPClient)

--- UDPServer.java
+++ UDPServer.java
@@ -1,61 +1,55 @@
-// Java program to illustrate Server side
-// Implementation using DatagramSocket
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 
-public class UDPServer
-{
-    public static void main(String[] args) throws IOException
-    {
-        // Step 1 : Create a socket to listen at port 1234
-        DatagramSocket ds = new DatagramSocket(1234);
+public class UDPServer {
+    public static void main(String[] args) throws IOException{
+
+        //Criando socket para receber dados da porta 1234 (usada pelo cliente para envio)
+        DatagramSocket datagrama = new DatagramSocket(1234);
+
+        //Criando array de bytes para usar no método "DatagramPacket"
         byte[] receive = new byte[65535];
 
-        DatagramPacket DpReceive = null;
+        DatagramPacket ReceivePacote = null;
         while (true) {
 
-            // Step 2 : create a DatgramPacket to receive the data.
-            DpReceive = new DatagramPacket(receive, receive.length);
+           //Criando pacote para receber os dados
+            ReceivePacote = new DatagramPacket(receive, receive.length);
 
-            // Step 3 : revieve the data in byte buffer.
-            ds.receive(DpReceive);
+            //Recebendo dados da porta 1234 e colocando no array de bytes
+            datagrama.receive(ReceivePacote);
 
-            System.out.println("Client:-" + data(receive));
+            System.out.println("Mensagem recebida!!> " + data(receive)+"\nEnviando ACK para o cliente...");
 
             //Enviando ACK
-
-            DatagramSocket ack = new DatagramSocket();
+            DatagramSocket pacote = new DatagramSocket();//Cria socket para enviar dados
             InetAddress ip = InetAddress.getLocalHost();
-            String a = String.valueOf(data(receive));
+            String mensagem = String.valueOf(data(receive));
 
-            DatagramPacket DpSend =
-                    new DatagramPacket(a.getBytes(), a.length(), ip, 1235);
 
-            ack.send(DpSend);
-            ack.close();
-            // Exit the server if the client sends "bye"
+            DatagramPacket ack = new DatagramPacket(mensagem.getBytes(), mensagem.length(), ip, 1235);
+
+            pacote.send(ack);//Enviando ACK de fato
+            pacote.close();//Fechando conexão com porta 1235 para evitar exception de porta sendo usada;
+
+            //Para de executar caso o cliente envie 'bye'
             if (data(receive).toString().equals("bye")) {
-                System.out.println("Client sent bye.....EXITING");
+                System.out.println("Cliente encerrou a conexão!");
                 break;
             }
 
-            // Clear the buffer after every message.
+            // Limpa o buffer depois de enviar o ACK
             receive = new byte[65535];
         }
     }
 
-    // A utility method to convert the byte array
-    // data into a string representation.
+    //Método para converter um array de bytes em string
     public static StringBuilder data(byte[] a) {
         if (a == null)
             return null;
         StringBuilder ret = new StringBuilder();
         int i = 0;
-        while (a[i] != 0)
-        {
+        while (a[i] != 0){
             ret.append((char) a[i]);
             i++;
         }
