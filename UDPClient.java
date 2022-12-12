78c7b9ee40c22882709bb481c8487b61a33b4987
André Oliveira <58999171+AndreOliveira23@users.noreply.github.com>
2022-12-12 16:13:54 -0300

    Update UDPClient.java
    Versão 3 do Cliente. Primeiras implementações de simulações. Essa versão (executando junto com a versão 3 do servidor) pode simular perda de pacotes, cálculo de atrasos de propagação e simulação de diferentes taxas de transmissão

--- UDPClient.java
+++ UDPClient.java
@@ -1,32 +1,34 @@
-import java.io.IOException;
 import java.net.*;
+import java.util.*;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
 
-public class UDPClient {
+
+public class UDPClient2 {
     public static void main(String args[]) throws IOException{
-        Scanner teclado = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);//Input do usuário
 
-        //Criando socket para enviar dados (mensagem)
-        DatagramSocket datagrama = new DatagramSocket();
+        DatagramSocket datagrama = new DatagramSocket();//Criando socket para enviar dados (mensagem)
 
-        InetAddress ip = InetAddress.getLocalHost();
+        InetAddress ip = InetAddress.getLocalHost();//IP = 127.0.0.1 (Localhost)
+
         byte buffer[] = null;
 
-        //Loop enquanto cliente não enviar "bye"
+        //Loop enquanto cliente não encerrar a conexão
         while (true){
             String input = teclado.nextLine();
 
             /*Convertendo String em array de Bytes
-            *(Construtor do método "DatagramPacket" requer array de bytes como primeiro parâmetro)
-            * */
+             *(Construtor do método "DatagramPacket" requer array de bytes como primeiro parâmetro)
+             * */
             buffer = input.getBytes();
 
-           //Criando datagrama para enviar os dados (mensagem) pela porta 1234
+            //Criando datagrama para enviar os dados (mensagem) pela porta 1234
             DatagramPacket pacote = new DatagramPacket(buffer, buffer.length, ip, 1234);
 
             //Enviando dados
             datagrama.send(pacote);
+
 
             //Recebendo ACK
             DatagramSocket ack = new DatagramSocket(1235);//Conectando ao socket de envio e recebimento de ack
@@ -36,16 +38,41 @@ public class UDPClient {
             ReceivePacote = new DatagramPacket(receive, receive.length);//Colocando conteudo do ack na variável
             ack.receive(ReceivePacote);//Recebendo ack via socket 1235
             String texto = new String(ReceivePacote.getData(), StandardCharsets.UTF_8);//Decodificando com UTF-8
-
+            Random random = new Random();
             /*
             /Printando apenas as mensagems (tirar o método trim() faz com que
             caracteres extras não pertinentes ao ocnteúdo da mensagem sejam impressos
              */
-            System.out.println("Ack Recebido! A mensagem '"+texto.trim()+"' chegou no servidor com sucesso!");
+            if(texto.contains("perdido")){//Simulação de perda de pacote
+                System.out.println("TIMEOUT!! ACK não recebido. Pacote perdido, precisa reenviar...");
+            } else if (texto.contains("Calculando")) {
 
+                int n = random.nextInt(3);
+                int distancia = random.nextInt(100);
+                float velocidade = random.nextFloat()*10;
+                System.out.println("Distancia: "+distancia+" km"); // 10km = 10000 m
+                System.out.printf("Velocidade: %.2f ms",velocidade);
+                System.out.println("\nAtraso de propagação (d/v) = ");
+
+            } else if (texto.contains("taxas")) {
+
+                int tamanhoDoPacote = random.nextInt(2000);
+                System.out.println("Tamanho do pacote: "+tamanhoDoPacote+" Bytes");
+                int[] taxas = new int[5];
+
+                for(int i=0;i<5;i++){
+                    taxas[i] = random.nextInt(100);
+                    double tempoDeTransmissão = (((tamanhoDoPacote*8)/(taxas[i]))*0.001);
+                    System.out.printf("Com uma taxa de tranmissão de %d Kbps, o pacote levaria %.3f segundos pra ser transmitido\n",+taxas[i],tempoDeTransmissão);
+                }
+
+            } else {
+                System.out.println("Ack Recebido! A mensagem '" + texto.trim() + "' chegou no servidor com sucesso!");
+            }
+            
             /*Fechando conexão para liberar a porta
-            *->Toda vez que o servidor enviar um ack para o cliente, usa-se a mesma porta;
-            * Se a conexão não for fechada, a classe lança uma exceção, indicando que a porta ainda está sendo usada
+             *->Toda vez que o servidor enviar um ack para o cliente, usa-se a mesma porta;
+             * Se a conexão não for fechada, a classe lança uma exceção, indicando que a porta ainda está sendo usada
              */
             ack.close();
 
