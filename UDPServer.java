423a172d92db2f1b44d3ce07cfc1536cfc2988aa
André Oliveira <58999171+AndreOliveira23@users.noreply.github.com>
2022-12-12 16:14:50 -0300

    Update UDPServer.java
    Versão 3 do Servidor. Primeiras implementações de simulações. Essa versão (executando junto com a versão 3 do cliente) pode simular perda de pacotes, cálculo de atrasos de propagação e simulação de diferentes taxas de transmissão

--- UDPServer.java
+++ UDPServer.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.net.*;
+import java.util.Random;
 
-public class UDPServer {
+public class UDPServer2 {
+
     public static void main(String[] args) throws IOException{
 
         //Criando socket para receber dados da porta 1234 (usada pelo cliente para envio)
@@ -10,10 +12,10 @@ public class UDPServer {
         //Criando array de bytes para usar no método "DatagramPacket"
         byte[] receive = new byte[65535];
 
-        DatagramPacket ReceivePacote = null;
+        DatagramPacket ReceivePacote;
         while (true) {
 
-           //Criando pacote para receber os dados
+            //Criando pacote para receber os dados
             ReceivePacote = new DatagramPacket(receive, receive.length);
 
             //Recebendo dados da porta 1234 e colocando no array de bytes
@@ -28,6 +30,51 @@ public class UDPServer {
 
 
             DatagramPacket ack = new DatagramPacket(mensagem.getBytes(), mensagem.length(), ip, 1235);
+
+            Random random = new Random();
+            int n = random.nextInt(10);
+
+            switch(n){
+                case 0:
+                    System.out.println("Simulação de perda de pacote: \n");
+                    mensagem = "ACK não recebido. Pacote perdido, precisa reenviar...";
+                    ack = new DatagramPacket(mensagem.getBytes(), mensagem.length(), ip, 1235);
+                    pacote.send(ack);//Enviando ACK de fato
+                    continue;
+
+                case 1:
+                    System.out.println("Simulação de atraso de propagação: \n");
+                    mensagem = "ACK recebido! Calculando atraso de propagação do pacote....";
+                    ack = new DatagramPacket(mensagem.getBytes(), mensagem.length(), ip, 1235);
+                    pacote.send(ack);//Enviando ACK de fato
+                    continue;
+
+                case 2:
+                    System.out.println("Simulação de roteador com fila e diferentes tamanhos: \n");
+
+                case 3:
+                    System.out.println("Simulação de diferentes taxas de transmissão: \n");
+                    /*
+                    Simular tamanho do pacote e calcular diferentes taxas
+                    ex: Tamanho do pacote: 1500B
+                    Taxa de tranmissão: 2Mbps, 5mbps
+                    "o pacote levaria x segundos pra ir do cliente ao servidor numa taxa de 2mbps
+                    "o pacote levaria y segundos para ir do cliente ao servidor numa taxa de 5mbps
+                     */
+                    mensagem = "ACK recebido! Simulando entregas com diferentes taxas e transmissão....";
+                    ack = new DatagramPacket(mensagem.getBytes(),mensagem.length(),ip,1235);
+                    pacote.send(ack);
+                    continue;
+
+                case 4:
+                    System.out.println("Simulação de compartilhamento de banda");
+
+                case 5:
+                    System.out.println("Simulação de buffer de recepção + lentidaão para leitura");
+
+            }
+
+
 
             pacote.send(ack);//Enviando ACK de fato
             pacote.close();//Fechando conexão com porta 1235 para evitar exception de porta sendo usada;
