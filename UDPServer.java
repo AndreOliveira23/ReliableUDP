4021a3bc997547d16fd82fffc0faed1df49c1065
André Oliveira <58999171+AndreOliveira23@users.noreply.github.com>
2022-12-19 18:05:03 -0300

    Update UDPServer,java - VERSÃO 7
    Versão 7 do server acompanhando versão 7 do client. Mudanças explicadas no read-me, com a mesma descrição do commit do client versão 7

--- UDPServer.java
+++ UDPServer.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class UDPServer {
 
     public static void main(String[] args) throws Exception {
+
         //Three-leg-handshaking com o cliente
         estabelecerConexao();//Conectando ao cliente
 
@@ -14,7 +15,6 @@ public class UDPServer {
 
         //Criando array de bytes para usar no método "DatagramPacket"
         byte[] receive = new byte[65535];
-
 
         while (true) { //Mantém a conexão ativa
 
@@ -30,97 +30,93 @@ public class UDPServer {
             String mensagem = String.valueOf(data(receive));
             DatagramPacket ack = null;
 
-            Random random = new Random();
-            int n = random.nextInt(7);
+            //Colocando cada palavra da mensagem numa posição do array
+            //A posicão 0 contém o código associado. Definido pelos cases 3,4 e 5 do UDPClient
+            String[] codigoDaSimulacao = mensagem.split(" ");
 
-            switch(n){
-                case 0:
-                    System.out.println("Simulando de perda de pacote: \n");
-                    System.out.println("===============================================================");
-                    mensagem = "TIMEOUT!! ACK não recebido. Pacote perdido, precisa reenviar...";//Mensagem a ser enviada para o cliente
-                    ack = new DatagramPacket(mensagem.getBytes(), mensagem.length(), ip, 1235);
-                    pacote.send(ack);//Enviando ACK de fato
-                    receive = new byte[65535];//Limpando o buffer
-                    continue;
-
-                case 1:
-                    System.out.println("Simulação de atraso de propagação: \n");
+            if(codigoDaSimulacao[0].contains("63a0a911a4ee7")) {
+                System.out.println("Simulando de perda de pacote: \n");
+                System.out.println("===============================================================");
+                mensagem = "TIMEOUT!! ACK não recebido. Pacote perdido, precisa reenviar...";//Mensagem a ser enviada para o cliente
+                ack = new DatagramPacket(mensagem.getBytes(), mensagem.length(), ip, 1235);
+                Thread.sleep(4000);
+                pacote.send(ack);//Enviando ACK de fato
+                receive = new byte[65535];//Limpando o buffer
 
 
-                    System.out.println("Enviando ACK para o cliente....");
-                    System.out.println("===============================================================");
+            } else if (codigoDaSimulacao[0].contains("63a0a911a4f29")) {
+
+                System.out.println("Simulando de atraso de propagação: \n");
+                Random random = new Random();
+                int distancia = random.nextInt(100);
+                float velocidade = random.nextFloat() * 10;
+                System.out.println("Distância: " + distancia + " km"); // 10km = 10000 m
+                System.out.printf("Velocidade: %.2f ms", velocidade);
+                System.out.println("\nAtraso de propagação (d/v) = ");
+                System.out.print("Mensagem recebida: ");
+                String[] novaMensagem = new String[mensagem.length()-1];
+                for(int i=1;i<codigoDaSimulacao.length;i++) { //Loop passando pela mensagem completa (sem o código, que está no índice (0)
+                    novaMensagem[i-1] = codigoDaSimulacao[i]+" ";//String "nova mensagem" recebe a mensagem em si, sem o código)
+                    System.out.print(codigoDaSimulacao[i]+" ");
+                }
+                System.out.println("\nEnviando ACK para o cliente....");
+                System.out.println("===============================================================");
+                String mensagemReal = "";
+                for(int i = 0;i<codigoDaSimulacao.length;i++) mensagemReal += novaMensagem[i]+"";//Obtendo mensagem real para enviar ao cliente
+                mensagemReal = mensagemReal.replaceAll("null","");//Substituindo nulls por espaços vazios
+                ack = new DatagramPacket(mensagemReal.getBytes(), mensagemReal.length(), ip, 1235);
+                Thread.sleep(3000);
+                pacote.send(ack);//Enviando ACK de fato
+                pacote.close();
+                receive = new byte[65535];//Limpando o buffer
+                continue;
+
+            } else if (codigoDaSimulacao[0].contains("63a0a911a4f62")) {
+
+                System.out.println("Simulando de buffer de recepção + lentidão para leitura: ");
+                String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit " + data(receive);
+                String[] bufferSimulado = text.split(" ");
+                for (int i = 0; i < bufferSimulado.length; i++) {
+                    System.out.println("Buffer[" + i + "] = " + bufferSimulado[i]);
                     Thread.sleep(3000);
-                    mensagem = "ACK recebido! Calculando atraso de propagação do pacote....";
-                    ack = new DatagramPacket(mensagem.getBytes(), mensagem.length(), ip, 1235);
-                    pacote.send(ack);//Enviando ACK de fato
-                    receive = new byte[65535];//Limpando o buffer
-                    continue;
+                }
+                System.out.print("Mensagem recebida: ");
+                String[] novaMensagem = new String[mensagem.length() - 1];
+                for (int i = 9; i < bufferSimulado.length; i++){
+                    novaMensagem[i-9] = codigoDaSimulacao[i-8]+" ";
+                    System.out.print(bufferSimulado[i] + " ");
+                }
+                String mensagemReal = "";
+                for(int i = 0;i<codigoDaSimulacao.length;i++) mensagemReal += novaMensagem[i]+"";//Obtendo mensagem real para enviar ao cliente
+                mensagemReal = mensagemReal.replaceAll("null","");//Substituindo nulls por espaços vazios
 
-                case 2:
-                    System.out.println("Simulando roteador com fila e diferentes tamanhos: \n");
-                    mensagem = "ACK recebido! simulação de roteador com fila e diferentes tamanhos....";
-                    ack = new DatagramPacket(mensagem.getBytes(), mensagem.length(), ip, 1235);
-                    pacote.send(ack);//Enviando ACK de fato
-                    Thread.sleep(26000);
-                    System.out.println("Mensagem recebida! > "+String.valueOf(data(receive)));
-                    System.out.println("Enviando ACK para o cliente...");
-                    System.out.println("===============================================================");
-                    receive = new byte[65535];//Limpando o buffer
-                    continue;
+                System.out.println("\nEnviando ACK para o cliente....");
+                System.out.println("===============================================================");
+                mensagem = String.valueOf(data(receive));
+                ack = new DatagramPacket(mensagemReal.getBytes(),mensagemReal.length(),ip,1235);
+                Thread.sleep(3000);
+                pacote.send(ack);//Enviando ACK de fato
+                pacote.close();
+                receive = new byte[65535];
+                continue;
 
-                case 3:
-                    System.out.println("Simulando diferentes taxas de transmissão: \n");
-                    Thread.sleep(5000);
-                    System.out.println("Mensagem recebida: "+mensagem);
-                    System.out.println("Enviando ACK para o cliente....");
-                    System.out.println("===============================================================");
-                    mensagem = "ACK recebido! Simulando entregas com diferentes taxas e transmissão....";
-                    ack = new DatagramPacket(mensagem.getBytes(),mensagem.length(),ip,1235);
-                    pacote.send(ack);
-                    receive = new byte[65535];
-                    continue;
+            } else {
 
-                case 4:
-                    System.out.println("Simulação de compartilhamento de banda");
-                    mensagem = "ACK Recebido! Simulação de compartilhamento de banda aconteceu no servidor!";
-                    ack = new DatagramPacket(mensagem.getBytes(),mensagem.length(),ip,1235);
-                    pacote.send(ack);
-                    Thread.sleep(10500);
-                    System.out.println("Mensagem recebida >> "+data(receive));
-                    System.out.println("Enviando ACK para o cliente....");
-                    System.out.println("===============================================================");
-                    receive = new byte[65535];
-                    continue;
+                System.out.println("Mensagem recebida: " + data(receive));
+                System.out.println("Enviando ACK para o cliente....");
+                System.out.println("===============================================================");
+                mensagem = String.valueOf(data(receive));
+                ack = new DatagramPacket(mensagem.getBytes(), mensagem.length(), ip, 1235);
+                Thread.sleep(1500);
+                pacote.send(ack);
+                pacote.close();
+                receive = new byte[65535];
 
-                case 5:
-                    System.out.println("Simulando de buffer de recepção + lentidão para leitura: ");
-                    String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit "+data(receive);
-                    String[] bufferSimulado = text.split(" ");
-                    for (int i=0;i<bufferSimulado.length;i++){
-                        System.out.println("Buffer["+i+"] = "+bufferSimulado[i]);
-                        Thread.sleep(3000);
-                    }
-                    System.out.println("Mensagem recebida!!> " + data(receive)+"");
-                    System.out.println("Enviando ACK para o cliente....");
-                    System.out.println("===============================================================");
-                    mensagem = "(5)ACK Recebido! Simulação de buffer!";
-                    ack = new DatagramPacket(mensagem.getBytes(),mensagem.length(),ip,1235);
-                    pacote.send(ack);
-                    continue;
-
-                case 6:
-                    System.out.println("Mensagem recebida >> "+String.valueOf(data(receive)));
-                    System.out.println("Enviando ACK para o cliente....");
-                    System.out.println("===============================================================");
-                    mensagem = "ACK Recebido! a mensagem chegou com sucesso sem interferências!";
-                    ack = new DatagramPacket(mensagem.getBytes(),mensagem.length(),ip,1235);
-                    pacote.send(ack);
-                    continue;
             }
 
             pacote.close();//Fechando conexão com porta 1235 para evitar exception de porta sendo usada;
 
-            //Para de executar caso o cliente envie 'bye'
+            //Para de executar caso o cliente envie 'exit'
             if (data(receive).toString().equals("exit")) {
                 System.out.println("Cliente encerrou a conexão!");
                 break;
@@ -145,20 +141,19 @@ public class UDPServer {
     }
 
     public static void estabelecerConexao() throws Exception{
-    //Recebendo RTS do cliente
-    DatagramSocket RTS = new DatagramSocket(1232);
-    byte[] received = new byte[65535];
-    DatagramPacket RTSCliente = new DatagramPacket(received, received.length);
-    RTS.receive(RTSCliente);
-    Thread.sleep(3000);
-    System.out.println("Pedido de conexão recebido! \nEnviando CTS ao cliente para estabelecer a conexão...");
-    System.out.println("===============================================================");
-    //Enviando CTS
-    DatagramSocket socket = new DatagramSocket();//Cria socket para enviar dados
-    InetAddress ipv4 = InetAddress.getLocalHost();
-
-    String SynAck = "Conexão estabelecida!";
-    DatagramPacket CTS = new DatagramPacket(SynAck.getBytes(), SynAck.length(), ipv4, 1240);
-    socket.send(CTS);//Enviando ACK de fato
+        //Recebendo RTS do cliente
+        DatagramSocket RTS = new DatagramSocket(1272);
+        byte[] received = new byte[65535];
+        DatagramPacket RTSCliente = new DatagramPacket(received, received.length);
+        RTS.receive(RTSCliente);
+        Thread.sleep(3000);
+        System.out.println("Pedido de conexão recebido! \nEnviando CTS ao cliente para estabelecer a conexão...");
+        System.out.println("===============================================================");
+        //Enviando CTS
+        DatagramSocket socket = new DatagramSocket();//Cria socket para enviar dados
+        InetAddress ipv4 = InetAddress.getLocalHost();
+        String SynAck = "Conexão estabelecida com sucesso!\n===============================================================";
+        DatagramPacket CTS = new DatagramPacket(SynAck.getBytes(), SynAck.length(), ipv4, 1240);
+        socket.send(CTS);//Enviando ACK de fato
     }
 }
