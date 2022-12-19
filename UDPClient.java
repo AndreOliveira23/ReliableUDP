4850a2aeed6cff65da1cf5930f81fc6900aaa5c3
André Oliveira <58999171+AndreOliveira23@users.noreply.github.com>
2022-12-19 18:03:32 -0300

    Update UDPClient.java - VERSÃO 7
    Uma pequena mudança de abordagem.  Essa versão funciona igual a versão 6, porém com diferenças de implementação (explicadas no readme)
    ->Código mais organizado

--- UDPClient.java
+++ UDPClient.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 
 
 public class UDPClient {
+
     public static void main(String args[]) throws Exception {
         //Three-leg-handshaking com o servidor
         estabelecerConexao();//Conectando ao servidor
@@ -28,15 +29,115 @@ public class UDPClient {
             //Criando datagrama para enviar os dados (mensagem) pela porta 1234
             DatagramPacket pacote = new DatagramPacket(buffer, buffer.length, ip, 1234);
 
-            //Enviando dados
-            datagrama.send(pacote);
+            //Usando Random pra definir qual será a simulação feita.
+            Random random = new Random();
+
+            int n = random.nextInt(7);
 
             /*O pacote é enviado para o servidor. No servidor é usado Random pra decidir qual simulação será feita
                 O servidor envia mensagem para o cliente, e a partir disso, as simulações começam
              */
 
-        //==========================================================================================================
+            switch(n){
+                case 0:
+                    System.out.println("Simulando roteadores com filas e diferentes tamanhos: \n");
+                    System.out.println("Seu pacote saiu da máquina-cliente");
+                    Thread.sleep(2000);
+                    System.out.println("Pacote chegou no roteador R1, sendo o 5º na fila");
+                    Thread.sleep(2400);
+                    System.out.println("Roteador R1 enviou o pacote 1...");
+                    Thread.sleep(2700);
+                    System.out.println("Roteador R1 enviou o pacote 2...");
+                    Thread.sleep(2600);
+                    System.out.println("Roteador R1 enviou o pacote 3...");
+                    Thread.sleep(2900);
+                    System.out.println("Roteador R1 enviou o pacote 4...");
+                    Thread.sleep(2700);
+                    System.out.println("O Roteador R1 enviou o seu pacote para o Roteador R2!");
+                    Thread.sleep(2800);
+                    System.out.println("O pacote chegou no roteador R2, sendo o 3º na fila!");
+                    Thread.sleep(2600);
+                    System.out.println("Roteador R2 enviou o pacote 1...");
+                    Thread.sleep(2300);
+                    System.out.println("Roteador R2 enviou o pacote 2...");
+                    Thread.sleep(2200);
+                    System.out.println("O Roteador R2 enviou o seu pacote pacote para o servidor!");
+                    datagrama.send(pacote);
+                    break;
 
+                case 1:
+                    System.out.println("Simulando diferentes taxas de transmissão: \n");
+
+                    int tamanhoDoPacote = random.nextInt(2000);
+                    System.out.println("Tamanho do pacote: " + tamanhoDoPacote + " Bytes");
+                    int[] taxas = new int[5];
+
+                    for (int i = 0; i < 5; i++) {
+                        taxas[i] = 1 + random.nextInt(100);
+                        double tempoDeTransmissao = (((tamanhoDoPacote * 8) / (taxas[i])) * 0.001);
+                        System.out.printf("Com uma taxa de tranmissão de %d Kbps, o pacote levaria %.3f segundos pra ser transmitido\n", +taxas[i], tempoDeTransmissao);
+                    }
+                    System.out.println("Enviando pacote ao servidor...");
+                    datagrama.send(pacote);
+                    break;
+
+                case 2:
+                    System.out.println("Simulando compartilhamento de banda: \n");
+
+                    int tamanhoDaMensagem = input.length();
+                    String pacote1 = input.substring(0, tamanhoDaMensagem / 2);
+                    String pacote2 = input.substring((tamanhoDaMensagem / 2), tamanhoDaMensagem);
+                    System.out.println("Pacote1 = " + pacote1);
+                    System.out.println("Pacote2 = " + pacote2);
+                    int x = random.nextInt(2);
+                    if (x == 0) {
+                        System.out.println("Houve colisão de RTS entre os pacotes. Aplicando back-off exponencial\nno pacote 2 e reenviando pacote 1 ");
+                    } else {
+                        System.out.println("Não houve colisão de RTS. CTS obtido para o pacote 1");
+                    }
+                    System.out.println("Enviando pacote 1....");
+                    Thread.sleep(3000);
+                    System.out.println("Pacote 1 enviado!!");
+                    Thread.sleep(2000);
+                    System.out.println("CTS obtido para o pacote 2");
+                    Thread.sleep(3000);
+                    System.out.println("Enviando pacote 2....");
+                    Thread.sleep(1500);
+                    System.out.println("Pacote 2 enviado!!");
+                    datagrama.send(pacote);
+                    break;
+
+                case 3: //Simulação de perda de pacote (ocorre no servidor)
+
+                    input = "63a0a911a4ee7 "+input;
+                    buffer = input.getBytes();
+                    //Criando datagrama para enviar os dados (mensagem) pela porta 1234
+                    pacote = new DatagramPacket(buffer, buffer.length, ip, 1234);
+                    datagrama.send(pacote);
+                    break;
+
+                case 4: //Simulação de cálculo de atraso de propagação (ocorre no servidor)
+                    input = "63a0a911a4f29 "+input;
+                    buffer = input.getBytes();
+                    //Criando datagrama para enviar os dados (mensagem) pela porta 1234
+                    pacote = new DatagramPacket(buffer, buffer.length, ip, 1234);
+                    datagrama.send(pacote);
+                    break;
+
+                case 5: //Simulação de buffer de recepção (ocorre no servidor)
+                    input = "63a0a911a4f62 "+input;
+                    buffer = input.getBytes();
+                    //Criando datagrama para enviar os dados (mensagem) pela porta 1234
+                    pacote = new DatagramPacket(buffer, buffer.length, ip, 1234);
+                    datagrama.send(pacote);
+                    break;
+
+                default:
+                    datagrama.send(pacote);
+
+            }
+
+//==================================================================================================================================================
 
             //RECEBENDO ACK DO SERVIDOR
             DatagramSocket ack = new DatagramSocket(1235);//Conectando ao socket de envio e recebimento de ack
@@ -46,120 +147,28 @@ public class UDPClient {
             ReceivePacote = new DatagramPacket(receive, receive.length);//Colocando conteudo do ack na variável
             ack.receive(ReceivePacote);//Recebendo ack via socket 1235
             String texto = new String(ReceivePacote.getData(), StandardCharsets.UTF_8);//Decodificando texto enviado pelo servidor com UTF-8
-            Random random = new Random();
 
-            /*
-            /Printando apenas as mensagems (tirar o método trim() faz com que
-            caracteres extras não pertinentes ao ocnteúdo da mensagem sejam impressos
-             */
+            //Printando apenas as mensagems (tirar o método trim() faz com que
+            //caracteres extras não pertinentes ao ocnteúdo da mensagem sejam impressos
 
-            if (texto.contains("perdido")) {//Simulação de perda de pacote
-
-                Thread.sleep(2700);
-                System.out.println(texto.trim());
+            if (texto.contains("perdido")) { //Se o texto contém a palavra "perdido", signfica que é simulação de pacote perdido, então só precisa printar a mensagem enviada pelo servidor
+                System.out.println(texto.trim() + "\n===============================================================");
+                ack.close();
+            } else {
+                System.out.println("Ack Recebido! A mensagem '" + texto.trim() + "' chegou no servidor com sucesso!");
                 System.out.println("===============================================================");
 
-            } else if (texto.contains("Calculando")) { //Simulação de cálculo de atraso de propagação
-
-                System.out.println("Simulando cálculo de atraso de propagação....");
-                int n = random.nextInt(3);
-                int distancia = random.nextInt(100);
-                float velocidade = random.nextFloat() * 10;
-                System.out.println("Distância: " + distancia + " km"); // 10km = 10000 m
-                System.out.printf("Velocidade: %.2f ms", velocidade);
-                System.out.println("\nAtraso de propagação (d/v) = ");
-                System.out.println("===============================================================");
-
-            } else if (texto.contains("roteador")) {//Simulação de roteador com fila
-
-                System.out.println("Seu pacote saiu da máquina-cliente");
-                Thread.sleep(2000);
-                System.out.println("Pacote chegou no roteador R1, sendo o 5º na fila");
-                Thread.sleep(2400);
-                System.out.println("Roteador R1 enviou o pacote 1");
-                Thread.sleep(2700);
-                System.out.println("Roteador R1 enviou o pacote 2");
-                Thread.sleep(2600);
-                System.out.println("Roteador R1 enviou o pacote 3");
-                Thread.sleep(2900);
-                System.out.println("Roteador R1 enviou o pacote 4");
-                Thread.sleep(2700);
-                System.out.println("O Roteador R1 enviou o seu pacote para o Roteador R2");
-                Thread.sleep(2800);
-                System.out.println("O pacote chegou no roteador R2, sendo o 3º na fila");
-                Thread.sleep(2600);
-                System.out.println("Roteador R2 enviou o pacote 1");
-                Thread.sleep(2300);
-                System.out.println("Roteador R2 enviou o pacote 2");
-                Thread.sleep(2200);
-                System.out.println("O Roteador R2 enviou o seu pacote pacote para o servidor!");
-                Thread.sleep(4000);
-                System.out.println("Ack recebido!! A mensagem '" + input + "' chegou no servidor com sucesso!");
-                System.out.println("===============================================================");
-
-            } else if (texto.contains("taxas")) { //Simulação de diferentes taxas de transmissão
-
-                int tamanhoDoPacote = random.nextInt(2000);
-                System.out.println("Tamanho do pacote: " + tamanhoDoPacote + " Bytes");
-                int[] taxas = new int[5];
-
-                for (int i = 0; i < 5; i++) {
-                    taxas[i] = 1 + random.nextInt(100);
-                    double tempoDeTransmissão = (((tamanhoDoPacote * 8) / (taxas[i])) * 0.001);
-                    System.out.printf("Com uma taxa de tranmissão de %d Kbps, o pacote levou %.3f segundos pra ser transmitido\n", +taxas[i], tempoDeTransmissão);
-                }
-                Thread.sleep(2000);
-                System.out.println("ACK recebido!! A mensagem '"+input+"' chegou no servidor com sucesso!");
-                System.out.println("===============================================================");
-
-
-            } else if (texto.contains("compartilhamento")) {
-                
-                int tamanhoDaMensagem = input.length();
-                String pacote1 = input.substring(0,tamanhoDaMensagem/2);
-                String pacote2 = input.substring((tamanhoDaMensagem/2),tamanhoDaMensagem);
-                System.out.println("Pacote1 = "+pacote1);
-                System.out.println("Pacote2 = "+pacote2);
-                int x = random.nextInt(2);
-                if(x == 0) {
-                    System.out.println("Houve colisão de RTS entre os pacotes. Aplicando back-off exponencial\nno pacote 2 e reenviando pacote 1 ");
-                }else {
-                    System.out.println("Não houve colisão de RTS. CTS obtido para o pacote 1");
-                }
-                System.out.println("Enviando pacote 1....");
-                Thread.sleep(3000);
-                System.out.println("Pacote 1 enviado!!");
-                Thread.sleep(2000);
-                System.out.println("CTS obtido para o pacote 2");
-                Thread.sleep(3000);
-                System.out.println("Enviando pacote 2....");
-                Thread.sleep(1500);
-                System.out.println("Pacote 2 enviado!!");
-                Thread.sleep(3000);
-                System.out.println("Ack Recebido! A mensagem '" + input + "' chegou no servidor com sucesso!");
-                System.out.println("===============================================================");
-
-            } else if (texto.contains("5")) {
-
-                System.out.println("ACK Recebido! A mensagem '"+ input +"' chegou no servidor com sucesso!");
-                System.out.println("===============================================================");
-
-
-            } else if (texto.contains("sucesso")) {
-                System.out.println("Ack Recebido! A mensagem '" + input + "' chegou no servidor com sucesso \n ( e sem interferências)!");
-                System.out.println("===============================================================");
+                /*Fechando conexão para liberar a porta
+                 *Toda vez que o servidor enviar um ack para o cliente, usa-se a mesma porta;
+                 * Se a conexão não for fechada, a classe lança uma exceção, indicando que a porta ainda está sendo usada
+                 */
+                ack.close();
             }
-
-            /*Fechando conexão para liberar a porta
-             *->Toda vez que o servidor enviar um ack para o cliente, usa-se a mesma porta;
-             * Se a conexão não for fechada, a classe lança uma exceção, indicando que a porta ainda está sendo usada
-             */
-            ack.close();
-
-            // Sai do loop caso o usuário digite "bye"
-            if (input.equals("exit")) break;
+                // Sai do loop caso o usuário digite "bye"
+                if (input.equals("exit")) break;
         }
     }
+
     public static void estabelecerConexao() throws Exception {
         Thread.sleep(1500);
         System.out.println("Tentando conectar ao servidor....");
@@ -167,7 +176,7 @@ public class UDPClient {
         InetAddress ip = InetAddress.getLocalHost();
         String RTS = "Tentando conectar ao servidor....";
         byte buffer[] = RTS.getBytes();
-        DatagramPacket pacote = new DatagramPacket(buffer, buffer.length, ip, 1232);
+        DatagramPacket pacote = new DatagramPacket(buffer, buffer.length, ip, 1272);
         datagrama.send(pacote);
         //Recebendo ACK do servidor
         DatagramSocket ack = new DatagramSocket(1240);//Conectando ao socket de envio e recebimento de ack
@@ -176,6 +185,6 @@ public class UDPClient {
         ReceivePacote = new DatagramPacket(receive, receive.length);//Colocando conteudo do ack na variável
         ack.receive(ReceivePacote);//Recebendo ack via socket 1235
         String CTS = new String(ReceivePacote.getData(), StandardCharsets.UTF_8);
-        if (CTS.contains("estabelecida")) Thread.sleep(3000);  System.out.println("Conexão estabelecida com sucesso!");
+        if (CTS.contains("estabelecida")) Thread.sleep(500);  System.out.println(CTS.trim());
     }
 }
